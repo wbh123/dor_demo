c20b138dff98e76b7436b92a1d052e55a404c53c
WangBH <2495680763@qq.com>
2026-08-01 16:30:27 +0800

    fix: 修改前端配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 5174,
+    allowedHosts: [
+      'ic22543aq11.vicp.fun'
+    ]
+  }
 })
