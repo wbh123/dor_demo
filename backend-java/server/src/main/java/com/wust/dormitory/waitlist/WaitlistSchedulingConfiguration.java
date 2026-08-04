b655269ceb9dd7f8b9ce5839a7b8c6d36db03e09
WangBinghao <2495680763@qq.com>
2026-08-04 14:45:00 +0800

    feat: enable waitlist scheduling

---
+++ backend-java/server/src/main/java/com/wust/dormitory/waitlist/WaitlistSchedulingConfiguration.java
@@ -0,0 +1,8 @@
+package com.wust.dormitory.waitlist;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class WaitlistSchedulingConfiguration { }
