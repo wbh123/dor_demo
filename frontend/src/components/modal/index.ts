59c492eab812150cfa74d4f78e87bdb216a88891
WangBinghao <2495680763@qq.com>
2026-08-05 10:51:20 +0800

    feat: 导出公共模态组件

---
+++ frontend/src/components/modal/index.ts
@@ -0,0 +1,4 @@
+export { default as AppModal } from './AppModal.vue'
+export { default as AppConfirmDialog } from './AppConfirmDialog.vue'
+export type { AppModalSize } from './AppModal.vue'
+export type { ConfirmDialogPayload, ConfirmDialogVariant } from './AppConfirmDialog.vue'
