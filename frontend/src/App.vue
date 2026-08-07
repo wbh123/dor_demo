870561ff535a530ecd4ebdee470e5175ead322dc
WangBinghao <2495680763@qq.com>
2026-08-07 19:23:24 +0800

    feat: 挂载管理员站点设置入口

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import AdminSiteSettingsShortcut from './components/admin/AdminSiteSettingsShortcut.vue'
+</script>
+
 <template>
   <RouterView />
+  <AdminSiteSettingsShortcut />
 </template>
