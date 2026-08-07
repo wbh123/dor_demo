17e2247c7f32e46f0642c2034cd2b48b57ea5ddc
WangBinghao <2495680763@qq.com>
2026-08-07 09:10:30 +0800

    feat: 添加组队选床成员接口

---
+++ backend-java/server/src/main/java/com/wust/dormitory/student/TeamSelectionMemberController.java
@@ -0,0 +1,28 @@
+package com.wust.dormitory.student;
+
+import com.wust.dormitory.common.response.ResponseFactory;
+import com.wust.dormitory.model.dto.ListSuccessResponse;
+import com.wust.dormitory.security.CurrentUser;
+import com.wust.dormitory.security.SecurityUsers;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/student/teams/{teamId}/selection-members")
+public class TeamSelectionMemberController {
+    private final TeamSelectionMemberService service;
+
+    public TeamSelectionMemberController(TeamSelectionMemberService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<ListSuccessResponse> list(@PathVariable long teamId) {
+        CurrentUser user = SecurityUsers.requireStudent();
+        return ResponseEntity.ok(ResponseFactory.list(
+                service.confirmedMembers(teamId, user.studentId())));
+    }
+}
