6317a19154b2a03b6975ff77e4abb7dc95b1ea70
WangBinghao <2495680763@qq.com>
2026-08-07 17:14:03 +0800

    refactor: 扩展学生管理详细查询Mapper

--- backend-java/server/src/main/java/com/wust/dormitory/admin/mapper/StudentAdminMapper.java
+++ backend-java/server/src/main/java/com/wust/dormitory/admin/mapper/StudentAdminMapper.java
@@ -1,6 +1,8 @@
 package com.wust.dormitory.admin.mapper;
 
+import com.wust.dormitory.admin.model.persistence.StudentAdminDetailRow;
 import com.wust.dormitory.admin.model.persistence.StudentCatalogRow;
+import com.wust.dormitory.admin.model.query.StudentAdminDetailQuery;
 import com.wust.dormitory.admin.model.query.StudentCatalogQuery;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -11,4 +13,8 @@ public interface StudentAdminMapper {
     long countStudents(StudentCatalogQuery query);
 
     List<StudentCatalogRow> findStudents(StudentCatalogQuery query);
+
+    long countDetailedStudents(StudentAdminDetailQuery query);
+
+    List<StudentAdminDetailRow> findDetailedStudents(StudentAdminDetailQuery query);
 }
